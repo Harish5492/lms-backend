a93383e39502781becc5fecb37d58cc53960070c
Your Name <you@example.com>
2024-01-31 10:26:40 +0530

    unique

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 const mongoDB = require('./config/database');
 
 const Api = require('./routers/index');
